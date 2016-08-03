96fcb6799b217fd84b7f806f816dcf1e8bbb4d52
Joel Yek <joevo2@gmail.com>
2016-08-03 18:41:18 +0800

    added messages view with data

--- application/components/messages/MessagesView.js
+++ application/components/messages/MessagesView.js
@@ -1,31 +1,74 @@
 import React, { Component } from 'react';
 import { 
-  View, 
-  Text
+    View, 
+    Text, 
+    Image, 
+    TouchableOpacity, 
+    ListView 
 } from 'react-native';
 
+import moment from 'moment';
+import { find } from 'underscore';
 import Icon from 'react-native-vector-icons/Ionicons';
 import NavigationBar from 'react-native-navbar';
 import Colors from '../../styles/colors';
-import { globals } from '../../styles';
+import { FakeConversations, FakeUsers, currentUser } from '../../fixtures';
+import { globals, messagesStyles } from '../../styles';
+import { rowHasChanged } from '../../utilities';
 
-class MessagesView extends Component{
-    render(){
+const styles = messagesStyles;
+
+class Conversations extends Component{
+    constructor(){
+        super();
+        this._renderRow = this._renderRow.bind(this);
+        this.dataSource = this.dataSource.bind(this);
+    }
+
+    _renderRow(conversation){
+        let otherUserID = find([conversation.user1Id, conversation.user2Id], (id) => id !== currentUser.id);
+        let user = find(FakeUsers, ({ id }) => id === otherUserID);
+        return (
+            <TouchableOpacity style={globals.flexContainer}>
+                <View style={globals.flexRow}>
+                    <Image style={globals.avatar} source={{uri: user.avatar}}/>
+                    <View style={globals.flex}>
+                        <View style={globals.textContainer}>
+                            <Text style={styles.h5}>{user.firstName} {user.lastName}</Text>
+                            <Text style={styles.h6}>{moment(conversation.lastMessageDate).fromNow()}</Text>
+                        </View>
+                        <Text style={styles.h4}>{conversation.lastMessageText.substring(0, 40)}...</Text>
+                    </View>
+                    <View style={styles.arrowContainer}>
+                        <Icon size={30} name="ios-arrow-forward" color={Colors.bodyTextLight}/>
+                    </View>
+                </View>
+        	    <View style={styles.divider}/>
+            </TouchableOpacity>
+        );
+    }
+    dataSource(){
+        return (
+            new ListView.DataSource({ rowHasChanged: rowHasChanged })
+            .cloneWithRows(FakeConversations)
+        );
+    }
+    render() {
         let titleConfig = { title: 'Messages', tintColor: 'white' };
         return (
             <View style={globals.flexContainer}>
                 <NavigationBar
-                title={titleConfig}
-                tintColor={Colors.brandPrimary}
+                    title={titleConfig}
+                    tintColor={Colors.brandPrimary}
                 />
-                <View style={globals.flexCenter}>
-                <Text style={globals.h2}>
-                    This is the MessagesView
-                </Text>
-                </View>
+                <ListView
+                    dataSource={this.dataSource()}
+                    contentInset={{ bottom: 49 }}
+                    renderRow={this._renderRow}
+                />
             </View>
-        )
-  }
+        );
+    }
 }
 
-export default MessagesView;
+export default Conversations;
