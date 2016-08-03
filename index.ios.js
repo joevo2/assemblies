fc1a734fd6ee6ffc8ba91e16f6489912bef5dc3a
Joel Yek <joevo2@gmail.com>
2016-08-03 16:16:32 +0800

    done with dashboard page

--- index.ios.js
+++ index.ios.js
@@ -5,11 +5,28 @@ import {
 } from 'react-native';
 
 import Landing from './application/components/Landing';
+import Dashboard from './application/components/Dashboard';
+import { globals } from './application/styles';
 
 class assemblies extends Component {
   render() {
     return (
-      <Landing />
+      <Navigator
+        style={globals.flex}
+        initialRoute={{ name: 'Landing'}}
+        renderScene={(route, navigator) => {
+          switch(route.name){
+            case 'Landing':
+              return (
+                <Landing navigator={navigator}/> 
+              );
+            case 'Dashboard':
+              return (
+                <Dashboard navigator={navigator}/>
+            );
+          }
+        }}
+      />
     );
   }
 }
