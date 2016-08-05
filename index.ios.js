b3481a5ab43ad9077c52643b2d96c72f3193fb3b
Joel Yek <joevo2@gmail.com>
2016-08-05 15:30:53 +0800

    done with login and register page

--- index.ios.js
+++ index.ios.js
@@ -7,6 +7,8 @@ import {
 import Landing from './application/components/Landing';
 import Dashboard from './application/components/Dashboard';
 import { globals } from './application/styles';
+import Register from './application/components/accounts/Register';
+import Login from './application/components/accounts/Login';
 
 class assemblies extends Component {
   render() {
@@ -23,6 +25,14 @@ class assemblies extends Component {
             case 'Dashboard':
               return (
                 <Dashboard navigator={navigator}/>
+            );
+            case 'Register':
+              return (
+                <Register navigator={navigator}/> 
+              );
+            case 'Login':
+              return (
+                <Login navigator={navigator}/>
             );
           }
         }}
